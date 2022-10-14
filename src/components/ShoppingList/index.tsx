da49198815a8fbe99b1c8a8c446dd2f8a435ffff
Amit Cesar <amitcesar27@gmail.com>
2022-10-14 12:54:46 -0300

    feat: getAll, getOne and changes in real time

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,50 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
-
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>();
+
+  const handleGetAllProducts = async () => {
+    const subscribe = firestore()
+      .collection("products")
+      .onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+        setProducts(data);
+      });
+    return () => subscribe();
+  };
+
+  const handleGetOneProduct = async () => {
+    firestore()
+      .collection("products")
+      .doc("4AR7fTm2I9rM4Rjz8RDT")
+      .get()
+      .then((response) => {
+        console.log({
+          id: response.id,
+          ...response.data(),
+        });
+      });
+  };
+
+  useEffect(() => {
+    handleGetAllProducts();
+    // handleGetOneProduct();
+  }, []);
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
